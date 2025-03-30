8e9f59c0993fe6bdbfd54ce55d1eebbcb7045c51
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-03-30 18:58:32 -0300

    Operadores lógicos

---
+++ opera_logico.py
@@ -0,0 +1,24 @@
+# Programa de eventos com AND:
+#idade = 30
+#altura = 1.79
+
+#resultado = (idade >= 20) and (altura >= 1.80)
+#msg = 'Pode participar do evento? ' + str(resultado)
+
+#print(msg)
+
+#Programa de alarme com OR:
+#porta = 'fechado'
+#janela = 'aberto'
+
+#alarme = (porta == 'aberto')  or (janela == 'aberto')
+
+#msg = 'Alarme disparado ?' +str(alarme)
+#print(msg)
+
+#Compra usando NOT:
+com_dinheiro = True
+com_dinheiro = not com_dinheiro
+
+msg = "Tem dinheiro ?" + str(com_dinheiro)
+print(msg)
