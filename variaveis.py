5035d3004a60b537661996bfd020654eb5b568c8
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-03-30 01:22:53 -0300

    Tipos de variáveis

---
+++ variaveis.py
@@ -0,0 +1,37 @@
+# DEclarando diferentes tipos de variáveis !
+nome = 'Rhoney'
+print(nome)
+
+nome_usuario = 'Ronaldo'
+print(nome_usuario)
+
+media = 0
+
+n1 = n2 = n3 = 1.2
+
+nome, idade = 'Rhoney', 42
+
+estado = True
+
+# função type()
+print(type(media))
+print(type(n1))
+print(type(nome))
+print(type(estado))
+print(type(1+2j)) # tipo de dado específico 
+
+# Fução isisntance()
+a = 10 
+b = 'sol'
+
+print(isinstance(a, int))
+print(isinstance(b, str))
+print(isinstance(a, float))
+print(isinstance(a, (int, float)))
+
+a = 40 
+c = 3
+r = a * c
+print(r)
+
+
