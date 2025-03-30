1caa02bd4a6326bb9031aaddc466df0b6decd8f7
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-03-30 19:42:04 -0300

    operados condicionais e modificações nos operadores aritiméticos

---
+++ condicionais.py
@@ -0,0 +1,14 @@
+n1 = n2 = 0.0
+media = 0.0
+
+n1 = float(input('Digite a primeira nota:'))
+n2 = float(input('Digite a segunda nota:'))
+
+#calculando a média:
+media = (n1 + n2) / 2
+
+if (media >= 7):
+    print('Resultado: Aprovado !')
+    print('Parabéns !!')
+
+print('Sua média é {}'.format(media))
